89768da56978e05e8cf6e485a439d7f6074ee308
Gashu12 <69815589+Gashu12@users.noreply.github.com>
2021-02-02 13:30:48 -0600

    Add files via upload

--- cityLibrary/js/library.js
+++ cityLibrary/js/library.js
@@ -5,7 +5,7 @@
  * @author Gashaw Kassa
  * @since 2021-02-01
  */
-/* 
+
 window.onload = libraryApp
 
 function libraryApp(){
@@ -17,18 +17,33 @@ function libraryApp(){
 async function fetchBooklists(){
 
     try{
-        let listOfBooks = await fetch("https://elibraryrestapi.herokuapp.com/elibrary/api/book/list")
+        let listOfBooks = await fetch("https://elibraryrestapi.herokuapp.com/elibrary/api/book/list",{
+            method: "GET"
+        })
         let res = await listOfBooks.json()
         console.log(res)
         let output = document.getElementById("booksTable")
-        for(let i = 0; i < res.length; i++){
-            output.innerHTML += "<td>" + res[i].bookId + "</td>"
-        + "<td>" + res[i+1].isbn + "</td>" + "<td>" + res[i+2].title + "</td>" +
-        "<td>" + res[i+3].overDueFee + "</td>" + "<td>" + res[i+4].publisher + "</td>" +
-        "<td>" + res[i+5].datePublished + "</td>"
+        for(let i = 0; i <= res.length; i++){
+            let count = 1
+            output.innerHTML += "<td>" + +(count+i) + "</td>"
+        + "<td>" + res[i].isbn + "</td>" + "<td>" + res[i].title + "</td>" +
+        "<td>" + res[i].overdueFee + "</td>" + "<td>" + res[i].publisher + "</td>" +
+        "<td>" + res[i].datePublished + "</td>"
         }
     } catch (err) {console.log(err)}
 
+
+}
+
+/* async function addBooks(){
+    try{
+        let addedForm = await fetch("https://elibraryrestapi.herokuapp.com/elibrary/api/book/add",{
+            method: "POST"
+        })
+        let form = await addedForm.text()
+        let outputForm = document.getElementById("form")
+        outputForm.innerHTML = form;
+    } catch (err) {console.log(err)}
 } */
 
 
